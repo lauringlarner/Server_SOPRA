9bad849c976f57c46d116554ce028327567bbdb2
Wallimann20-914-099 <naren.wallimann@uzh.ch>
2026-04-22 16:57:47 +0200

    changed back secret manager changes

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/PusherService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/PusherService.java
@@ -1,61 +1,29 @@
 package ch.uzh.ifi.hase.soprafs26.service;
 
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.pusher.rest.Pusher;
 
-import ch.uzh.ifi.hase.soprafs26.config.SecretManagerService;
-
-@Profile("!test")
 @Service
 public class PusherService {
 
     private final Pusher pusher;
 
-    private final Logger log = LoggerFactory.getLogger(GameService.class);
+    private final Logger log = LoggerFactory.getLogger(PusherService.class);
 
-    public PusherService(org.springframework.core.env.Environment env,
-                          SecretManagerService secrets) {
-
-        // look for env variables locally
-        String appId = env.getProperty("pusher.appId");
-        String key = env.getProperty("pusher.key");
-        String secret = env.getProperty("pusher.secret");
-        String cluster = env.getProperty("pusher.cluster");
-
-     
-        // if env variables missing locally -> use secret manager
-        if (isMissing(appId, key, secret, cluster)) {
-
-            String projectId = com.google.cloud.ServiceOptions.getDefaultProjectId();
-            if (projectId == null || projectId.isBlank()) {
-                log.debug("GOOGLE_CLOUD_PROJECT resulted in faulty projectId {}", projectId);
-
-                throw new IllegalStateException(
-                    "GOOGLE_CLOUD_PROJECT is not set. required for secret manager"); 
-            }  
-
-            appId = Objects.requireNonNull(secrets.getSecret(projectId, "pusher-app-id"));
-            key = Objects.requireNonNull(secrets.getSecret(projectId, "pusher-key"));
-            secret = Objects.requireNonNull(secrets.getSecret(projectId, "pusher-secret"));
-            cluster = Objects.requireNonNull(secrets.getSecret(projectId, "pusher-cluster"));
-        }
+    public PusherService(
+            @Value("${pusher.appId}") String appId,
+            @Value("${pusher.key}") String key,
+            @Value("${pusher.secret}") String secret,
+            @Value("${pusher.cluster}") String cluster
+        ) {
 
         this.pusher = new Pusher(appId, key, secret);
         this.pusher.setCluster(cluster);
         this.pusher.setEncrypted(true);
-    }
-
-    private boolean isMissing(String... values) {
-        for (String v : values) {
-            if (v == null || v.isBlank()) return true;
-        }
-        return false;
     }
 
     public void trigger(String channel, String event, Object payload) {
